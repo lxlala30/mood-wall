a2b6e632a1606aaa7c8104521e789fe99e9a6da9
lx30 <1923035819@qq.com>
2026-03-09 21:46:37 +0800

    修改系统重定向路由跳转操作：/ -》index.html；增加路由重定向控制实现类；以及优化各部分文件名称并路由适配；

--- src/main/java/com/example/tool/config/WebMvcConfig.java
+++ src/main/java/com/example/tool/config/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package com.example.tool.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -9,11 +12,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebMvcConfig.class);
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 映射/uploads目录为静态资源
         String uploadPath = "file:" + System.getProperty("user.dir") + "/uploads/";
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations(uploadPath);
+    }
+
+    // 通过实现 WebMvcConfigurer 接口自定义 MVC 配置，适合需要批量配置多个规则的场景
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        logger.info("addViewControllers() -> index path:{}", "http://118.25.94.106:8082/index.html");
+        // 把根路径 "/" 映射到 tool_server.html
+        registry.addViewController("/no").setViewName("forward:/nono.html");
+        // 可选：设置优先级（数字越小优先级越高）
+        registry.setOrder(0);
     }
 }
---
+++ src/main/java/com/example/tool/controller/IndexController.java
@@ -0,0 +1,38 @@
+package com.example.tool.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    /**
+     * 根路径 "/" 重定向到 tool_server.html
+     */
+    @GetMapping("/12")
+    public String redirectToIndex() {
+        // 方式 1：内部转发（地址栏不变）
+        String path = "forward:/onetwo.html";
+        logger.info("redirectToIndex() -> path:{}", path);
+
+        // 方式 2：重定向（地址栏会显示 /tool_server.html）
+        // return "redirect:/tool_server.html";
+        return path;
+    }
+
+    /**
+     * 根路径 "/" 重定向到 tool_server.html
+     */
+    @GetMapping("/")
+    public String redirectToPath() {
+        // 方式 2：重定向（地址栏会显示 /index.html）
+//        String path = "redirect:/index.html";
+        String path = "forward:/index.html";
+        logger.info("redirectToIndex() -> path:{}", path);
+        return path;
+    }
+}
